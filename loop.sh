05313cc6b3f8228050764f958aed39071da34423
mike <mike.atkinson2019@gmail.com>
2022-07-28 21:44:45 +0200

    asa

--- loop.sh
+++ loop.sh
@@ -3,11 +3,28 @@ export DISPLAY=:0.0
 x=1
 RIGHT="xdotool mousemove 600 100 click 1"
 LEFT="xdotool mousemove 100 100 click 1"
+
+WAIT=10
+
 while [ $x -le 5 ]
 do
-  `$RIGHT`
-  `$RIGHT`
-  sleep 20
-  `$LEFT`
+  take_one
+  take_three
   x=$(( $x + 1 ))
 done
+
+
+
+take_one() {
+  `$LEFT`
+  `$RIGHT`
+  sleep $WAIT
+  `$LEFT`
+}
+
+take_three() {
+  `$LEFT`
+  `$LEFT`
+  sleep $WAIT
+  `$LEFT`
+}
