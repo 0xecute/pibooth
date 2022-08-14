30c5f7742acd35bfe863b71a48e95a6e4f9e8a6b
mike <mike.atkinson2019@gmail.com>
2022-08-14 21:00:33 +0200

    dsa

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -467,10 +467,11 @@ class ProcessingBackground(Background):
 
     def __init__(self):
         Background.__init__(self, "processing", color=None)
+        self._background_color = (255, 255, 255)
+        self._background_image = None
 
     def resize(self, screen):
-        self._background_color = (255,255,255)
-        self._background_image = None
+
         Background.resize(self, screen)
 
 
