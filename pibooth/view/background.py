ae26fd417a444144567b21f1ae6b42d05c2739d3
mike <mike.atkinson2019@gmail.com>
2022-08-14 20:26:24 +0200

    Add printing

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -158,7 +158,7 @@ class Background(object):
             overlay_name = "{}.png".format(self._name)
             if osp.isfile(pictures.get_filename(overlay_name)):
                 self._overlay = pictures.get_pygame_image(
-                    pictures.get_filename(overlay_name), (self._rect.width, self._rect.height))
+                    pictures.get_filename(overlay_name), (self._rect.width, self._rect.height), color=self._text_color)
 
             self.resize_texts()
             self._need_update = True
