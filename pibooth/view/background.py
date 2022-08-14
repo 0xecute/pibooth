c4c97175c048549fabfa937a8bb52ea22ad9871f
mike <mike.atkinson2019@gmail.com>
2022-08-14 20:39:02 +0200

    Add printing

--- pibooth/view/background.py
+++ pibooth/view/background.py
@@ -468,7 +468,7 @@ class ProcessingBackground(Background):
         Background.__init__(self, "processing")
 
     def resize(self, screen):
-        self._background_color = (255,255,255)
+        #self._background_color = (255,255,255)
         self._background_image = None
         Background.resize(self, screen)
 
