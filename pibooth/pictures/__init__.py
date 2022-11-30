d689f2a5fe8ee4874df11a3c7d365aaa4ef75e0c
mike <mike.atkinson2019@gmail.com>
2022-11-30 12:25:23 +0100

    Update buttons to be pull down

--- pibooth/pictures/__init__.py
+++ pibooth/pictures/__init__.py
@@ -121,6 +121,8 @@ def get_pygame_layout_image(text_color, bg_color, layout_number, size):
     """
     layout_image = get_pygame_image("layout{0}.png".format(layout_number),
                                     size, color=text_color, bg_color=bg_color)
+    if layout_number == 3:
+        layout_image = get_pygame_image("layout{0}.png".format(layout_number),size)
     text = language.get_translated_text(str(layout_number))
     if text:
         rect = layout_image.get_rect()
